5dd74c9d84d9fa0b07a0b6359483d4bee680e5ce
Pedro Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2023-10-24 19:20:21 -0300

    config: implementing spring

--- app/src/main/java/money/manager/App.java
+++ app/src/main/java/money/manager/App.java
@@ -3,8 +3,26 @@
  */
 package money.manager;
 
+import java.util.logging.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import money.manager.configuration.WebServerConfiguration;
+
+@SpringBootApplication
 public class App {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LOG.info("Starting money.manager... ");
+
+        SpringApplication.run(WebserverConfiguration.class, args);
+        
+        LOG.info("Ending of starting money.manager... ");
     }
 }
